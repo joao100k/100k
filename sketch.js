d28901cb9f5809b8e7ef509254e1dd2778acb886
joao100k <joao.pacheco.maximo@escola.pr.gov.br>
2024-09-19 08:10:46 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() { createCanvas(1200, 1200);background("black"); }
+
+function draw() {
+
+//console.log(mouseIsPressed); if(mouseIsPressed){ rect(mouseX, mouseY, 40, 85);
+
+}
+
+fill("red"); stroke("blue"); }
